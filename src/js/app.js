ad9273cfc09da44427bd72eead5485b69125050d
Denis Yefremenko <denvirus83@mail.ru>
2021-08-21 14:01:13 +0400

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+
+import hello, * as data from './hello.js';
+
+console.log('app started!!');
+
+hello('Sasha');
+
+console.log(data.myName);
---
+++ src/js/hello.js
@@ -0,0 +1,10 @@
+
+export const myName = 'Den';
+
+const sayhello = function (name) {
+  console.log(`${name}, hello!!`);
+};
+
+export default sayhello;
+
+console.log('worked');
